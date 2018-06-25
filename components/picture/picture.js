e37d4eecdc9b689fcf4c500c558f4200367e3028
hp <hupei@xfxb.net>
2018-06-25 18:16:11 +0800

    修改picture

--- components/picture/picture.js
+++ components/picture/picture.js
@@ -21,18 +21,18 @@ Component({
     bindPicture: function (e) {
       const that = this;
       wx.chooseImage({
-        count: 1, // 默认9
+        count: 9, // 默认9
         sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
         sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
         success: function (res) {
           // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
           var tempFilePaths = res.tempFilePaths
-          console.log(tempFilePaths[0]);
+          console.log(tempFilePaths);
           let imgList = 
           that.setData({
-              imgList: [...that.data.imgList, tempFilePaths[0]]
+              imgList: [...that.data.imgList, ...tempFilePaths]
           },()=>{
-            console.log(that.data.imgList);
+            // console.log(that.data.imgList);
           })
         }
       })
